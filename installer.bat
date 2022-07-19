85b6ce8cd5c8d4122f32bc7e005db3bf59729be0
BluePlattypus <97361820+BluePlattypus@users.noreply.github.com>
2022-07-19 14:58:27 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -7,5 +7,4 @@ powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%IPATH%\MultiMC.l
 cd "%AppData%\Microsoft\Windows\Start Menu\Programs"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%AppData%\Microsoft\Windows\Start Menu\Programs\MultiMC.lnk');$s.TargetPath=('%IPATH%\MultiMC\MultiMC.exe');$s.IconLocation=('%IPATH%\MultiMC\MultiMC.exe');$s.WorkingDirectory=('%IPATH%\MultiMC');$s.WindowStyle=1;$s.Save()"
 icacls "%IPATH%\MultiMC" /grant %Username%:(OI)(CI)F /T
-timeout /t 100
 type "Successfully installed!"
