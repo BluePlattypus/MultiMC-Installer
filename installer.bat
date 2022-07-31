547d82ec5f07de1e730a28d533d7c26778ccdb67
BluePlattypus <97361820+BluePlattypus@users.noreply.github.com>
2022-07-31 13:49:34 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -3,7 +3,6 @@ cd "%IPATH%"
 curl "https://files.multimc.org/downloads/mmc-stable-win32.zip" --ssl-no-revoke -O
 tar -xf "mmc-stable-win32.zip"
 del "mmc-stable-win32.zip"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%IPATH%\MultiMC.lnk');$s.TargetPath=('%IPATH%\MultiMC\MultiMC.exe');$s.IconLocation=('%IPATH%\MultiMC\MultiMC.exe');$s.WorkingDirectory=('%IPATH%\MultiMC');$s.WindowStyle=1;$s.Save()"
 cd "%AppData%\Microsoft\Windows\Start Menu\Programs"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%AppData%\Microsoft\Windows\Start Menu\Programs\MultiMC.lnk');$s.TargetPath=('%IPATH%\MultiMC\MultiMC.exe');$s.IconLocation=('%IPATH%\MultiMC\MultiMC.exe');$s.WorkingDirectory=('%IPATH%\MultiMC');$s.WindowStyle=1;$s.Save()"
 icacls "%IPATH%\MultiMC" /grant %Username%:(OI)(CI)F /T
